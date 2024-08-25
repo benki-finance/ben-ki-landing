7c98c9abaccc54a02b1e03a637f553280cf1d123
abrarfrahman <abrarfrahman@gmail.com>
2024-08-25 01:52:54 -0700

    replaced company logos

--- components/business-categories.tsx
+++ components/business-categories.tsx
@@ -1,5 +1,4 @@
 import Image from "next/image";
-// TODO: change to benki logos
 import Logo01 from "@/public/images/logo-01.svg";
 import Logo02 from "@/public/images/logo-02.svg";
 import Logo03 from "@/public/images/logo-03.svg";
@@ -120,7 +119,7 @@ export default function BusinessCategories() {
                 />
               </svg>
             </div>
-            
+
             {/* Green glow */}
             <div className="absolute -z-10">
               <svg
