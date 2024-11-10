6164f3f744c36f97d3327585d4c1e212add196a7
abrarfrahman <abrarfrahman@gmail.com>
2024-11-09 23:25:09 -0800

    updated images

--- components/zigzag.tsx
+++ components/zigzag.tsx
@@ -2,6 +2,7 @@ import Image from 'next/image'
 
 import FeatImage01 from '@/public/images/wf-01.png'
 import FeatImage02 from '@/public/images/wf-02.png'
+import FeatImage03 from '@/public/images/wf-03.png'
 
 export default function Zigzag() {
   return (
@@ -20,26 +21,26 @@ export default function Zigzag() {
               {/* Content */}
               <div className="max-w-xl md:max-w-none md:w-full mx-auto md:col-span-7 lg:col-span-6" data-aos="fade-left">
                 <div className="md:pl-4 lg:pl-12 xl:pl-16">
-                  <h2 className="text-2xl font-bold text-emerald-600 mb-2">Assemble Your Dynamic CIM Draft</h2>
+                  <h2 className="text-2xl font-bold text-emerald-600 mb-2">Dynamic, Auditable Deal Docs</h2>
                   <ul className="text-lg text-gray-400 -mb-2">
                     <li className="flex items-center mb-2">
                       <svg className="w-3 h-3 fill-current text-emerald-500 mr-2 shrink-0" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
                         <path d="M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z" />
                       </svg>
-                      <span>Simply upload new data, and changes to subsequent sections cascade through your memo</span>
+                      <span>Instantly generate deal documents with AI and update financial models</span>
                     </li>
                     <li className="flex items-center mb-2">
                       <svg className="w-3 h-3 fill-current text-emerald-500 mr-2 shrink-0" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
                         <path d="M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z" />
                       </svg>
-                      <span>Harmonize your expertise with AI to create a living memo that learns with every adjustment</span>
+                      <span>Reduce compliance risk by tracking human and AI contributions alike</span>
                     </li>
                   </ul>
                 </div>
               </div>
             </div>
 
-            {/* HOME DASHBOARD */}
+            {/* MODEL STEERING */}
             <div className="md:grid md:grid-cols-12 md:gap-6 items-center">
               {/* Image */}
               <div className="max-w-xl md:max-w-none md:w-full mx-auto md:col-span-5 lg:col-span-6 mb-8 md:mb-0 md:order-1" data-aos="fade-up">
@@ -48,25 +49,47 @@ export default function Zigzag() {
               {/* Content */}
               <div className="max-w-xl md:max-w-none md:w-full mx-auto md:col-span-7 lg:col-span-6" data-aos="fade-right">
                 <div className="md:pr-4 lg:pr-12 xl:pr-16">
-                  <h2 className="text-2xl font-bold text-emerald-600 mb-2">Supercharge Your Data Silos</h2>
+                  <h2 className="text-2xl font-bold text-emerald-600 mb-2">Unmatched Control Over Financial AI</h2>
                   <ul className="text-lg text-gray-400 -mb-2">
                     <li className="flex items-center mb-2">
                       <svg className="w-3 h-3 fill-current text-emerald-500 mr-2 shrink-0" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
                         <path d="M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z" />
                       </svg>
-                      <span>Access and manage all your deal templates</span>
+                      <span>Create AI agents in real-time with model customization playground</span>
                     </li>
                     <li className="flex items-center mb-2">
                       <svg className="w-3 h-3 fill-current text-emerald-500 mr-2 shrink-0" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
                         <path d="M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z" />
                       </svg>
-                      <span>Configure data sources, access, and role assignments</span>
+                      <span>Integrate custom knowledge sources, access permissions, and role assignments for virtual analysts</span>
                     </li>
-                    <li className="flex items-center">
+                  </ul>
+                </div>
+              </div>
+            </div>
+
+            {/* CIM DRAFT TEMPLATE VIEW */}
+            <div className="md:grid md:grid-cols-12 md:gap-6 items-center">
+              {/* Image */}
+              <div className="max-w-xl md:max-w-none md:w-full mx-auto md:col-span-5 lg:col-span-6 mb-8 md:mb-0 rtl" data-aos="fade-up">
+                <Image className="max-w-full mx-auto md:max-w-none h-auto" src={FeatImage03} width={540} height={405} alt="Features 03" />
+              </div>
+              {/* Content */}
+              <div className="max-w-xl md:max-w-none md:w-full mx-auto md:col-span-7 lg:col-span-6" data-aos="fade-left">
+                <div className="md:pl-4 lg:pl-12 xl:pl-16">
+                  <h2 className="text-2xl font-bold text-emerald-600 mb-2">LLM Benchmarking for Finance</h2>
+                  <ul className="text-lg text-gray-400 -mb-2">
+                    <li className="flex items-center mb-2">
                       <svg className="w-3 h-3 fill-current text-emerald-500 mr-2 shrink-0" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
                         <path d="M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z" />
                       </svg>
-                      <span>Track your deal flow and generate reports</span>
+                      <span>FinBench Arena: an open-source platform enabling transparent evaluation for real-world financial scenarios</span>
+                    </li>
+                    <li className="flex items-center mb-2">
+                      <svg className="w-3 h-3 fill-current text-emerald-500 mr-2 shrink-0" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z" />
+                      </svg>
+                      <span>Build an LLM deployment strategy for your firm's approach and risk tolerance</span>
                     </li>
                   </ul>
                 </div>
